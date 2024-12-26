18f6dab8717b4f65a36387b7febb0fa47716d260
big32mike <michaeljwhite@gmail.com>
2024-12-26 15:00:29 -0500

    Clock tick at 60fps and assign dt

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
